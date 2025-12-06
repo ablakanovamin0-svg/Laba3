a2fa86aebd78a6f61c7ebc808043914d94905a27
ablakanovamin0-svg <ablakanovamin0@gmail.com>
2025-12-06 21:49:26 +0300

    Создание метода containsTime

--- src/main/java/org/example/Comparison.java
+++ src/main/java/org/example/Comparison.java
@@ -89,4 +89,14 @@ public class Comparison {
         }
         return (System.nanoTime() - startTime) / 1000000.0;
     }
+    private double containsTime(List<Integer> list) {
+        for (int i = 0; i < count; i++) {
+            list.add(i);
+        }
+        long startTime = System.nanoTime();
+        for (int i = 0; i < count; i++) {
+            list.contains(i);
+        }
+        return (System.nanoTime() - startTime) / 1000000.0;
+    }
 }
