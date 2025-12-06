99ff5ebc731f7537a3b288d80400a2637c63c875
ablakanovamin0-svg <ablakanovamin0@gmail.com>
2025-12-06 21:41:15 +0300

    Создание классов Tests и Comparison

---
+++ src/main/java/org/example/Comparison.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Comparison {
+    private int count;
+    public Comparison() {
+        count = 1000;
+    }
+    public Comparison(int operationCount) {
+        count = operationCount;
+    }
+
+}
